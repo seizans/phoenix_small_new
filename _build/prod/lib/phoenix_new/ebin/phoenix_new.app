babf5d022ff27e9dba656655ee237930cf72b62a
Seizan Shimazaki <seizans@gmail.com>
2016-08-04 17:18:37 +0900

    Clone phoenix/installer

---
+++ _build/prod/lib/phoenix_new/ebin/phoenix_new.app
@@ -0,0 +1,7 @@
+{application,phoenix_new,
+             [{registered,[]},
+              {description,"phoenix_new"},
+              {vsn,"1.2.0"},
+              {modules,['Elixir.Mix.Tasks.Local.Phoenix',
+                        'Elixir.Mix.Tasks.Phoenix.New']},
+              {applications,[kernel,stdlib,elixir]}]}.
---
+++ mix.exs
@@ -0,0 +1,16 @@
+defmodule Phoenix.New.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :phoenix_new,
+     version: "1.2.0",
+     elixir: "~> 1.2"]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: []]
+  end
+end
